bcba2af21ac163ed1b395a01dfe20a2e1c760fd8
aditipolkam <aditipolkam@gmail.com>
2021-08-24 14:04:22 +0530

    MODIFIED program for Bubble Sort algorithm

--- Sorting Algorithms/bubblesort.cpp
+++ Sorting Algorithms/bubblesort.cpp
@@ -2,7 +2,20 @@
 using namespace std;
 int main()
 {
-    int num[] = {33,15,8,7,6,4,234,3,23,24,98,6};
-    
+    int num[12] = {33,15,8,7,6,4,234,3,23,24,98,6};
+    int size = 12;
+    cout<<size<<"\n";
+    for(int i=0; i<size-1; i++){
+        for(int j=0; j<size-i-1; j++){
+            if(num[j]>num[j+1]){
+                int temp = num[j];
+                num[j] = num[j+1];
+                num[j+1] = temp;
+            }
+        }
+    }
+    for(int i=0; i<size; i++){
+        cout<<num[i]<<"\t";
+    }
     return 0;
 }
